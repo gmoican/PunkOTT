59ea8077bf9d627d21e63d77a602779acb7528c8
Guillermo Moñino Cánovas <guillermomcanovas@gmail.com>
2025-12-08 13:26:26 +0100

    trying to fix build workflow

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -1,5 +1,5 @@
 #include "helpers/test_helpers.h"
-#include <PunkOTTProcessor.h>
+#include <PluginProcessor.h>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
